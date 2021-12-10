a5183434bcd8d0a3a9d96ab7414a5946b795a32e
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-12-10 17:32:11 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ mybutton = document.getElementById("top-btn");
 // [When the user scrolls down 20px from the top of the document, show the button]
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-     if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
+     if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
        mybutton.style.display = "flex";} 
        else {
        mybutton.style.display = "none";}
