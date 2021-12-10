49d5f7e5531da60e9ebb8b3c6cfafd82fea6f143
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-12-10 02:35:07 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,3 @@
-function scale(x) {
-  if (x.matches) { // If media query matches
-    document.getElementById('menu-toggle').style.display = 'flex';
-  } else {
-    document.getElementById('menu-toggle').style.display = 'none';
-  }}
-var x = window.matchMedia("(max-width: 950px)")
-scale(x) // Call listener function at run time
-x.addListener(scale) // Attach listener function on state changes 
-const menu = document.getElementById('menu-toggle');
-const menupoint = document.getElementById('menupointid');
-const navUL = document.getElementById('nav-ul');
-const navMenu = document.getElementById('navMenu');
-const address = document.getElementById('address');
-const body = document.getElementById('bodyid');
-menu.addEventListener('click', () => {
-      navMenu.classList.toggle('show');
-      navUL.classList.toggle('show');
-      address.classList.toggle('show');
-      body.classList.toggle('show');
-      menu.classList.toggle('show');
-      menupoint.classList.toggle('show');
-}); 
 //TO TOP BUTTON
 mybutton = document.getElementById("myBtn");
 // [When the user scrolls down 20px from the top of the document, show the button]
