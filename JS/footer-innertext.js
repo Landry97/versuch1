576d0f5a898fad111b0f2a64c6e65916d9e5e9a8
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-12-14 09:34:19 +0100

    Update footer-innertext.js

--- JS/footer-innertext.js
+++ JS/footer-innertext.js
@@ -1,18 +1,15 @@
-var windowsize = window.matchMedia( "(min-width: 1300px)" );
-if (windowsize.matches) {
+function scale(windowsize) {
+  if (windowsize.matches) { // If media query matches
+    
 document.getElementById("impressum-id").innerHTML = "IMPRESSUM";
 document.getElementById("sponsoren-id").innerHTML = "SPONSOREN";
 document.getElementById("daten-id").innerHTML = "DATENSCHUTZERKLÄRUNG";
 document.getElementById("team-id").innerHTML = "TEAM";
 document.getElementById("instagram-id").innerHTML = "INSTAGRAM";
-}
-else{
-  
+  } else {
 document.getElementById("impressum-id").innerHTML = 'Impressum';
 document.getElementById("sponsoren-id").innerHTML = 'Sponsoren';
 document.getElementById("daten-id").innerHTML = 'Datenschutzerklärung';
 document.getElementById("team-id").innerHTML = 'Team';
 document.getElementById("instagram-id").innerHTML = 'Instagram';
-};
-scale(windowsize) // Call listener function at run time
-windowsize.addListener(scale) // Attach listener function on state changes
+  }};
